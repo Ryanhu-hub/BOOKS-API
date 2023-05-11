e6ad2fa7a9f704d833d5071c246e4fcd0a4ab82d
Ryanhu-hub <ryanhuot20@gmail.com>
2023-05-10 17:25:44 -0700

    Index route added to controller

--- controller/books-controller.js
+++ controller/books-controller.js
@@ -3,49 +3,23 @@ const Books = require('../models/books')
 //  const db = require('../models')
 const books = require('../models/books')
 
-// router.get('/seed', (req, res) => {
-//     books.insertMany([{
-//         "title": "The Shinobi Initiative",
-//         "description": "The reality-bending adventures of a clandestine service agency in the year 2166",
-//         "year": 2014,
-//         "quantity": 10,
-//         "imageURL": "https://imgur.com/LEqsHy5.jpeg"
-//       },
-//       {
-//         "title": "Tess the Wonder Dog",
-//         "description": "The tale of a dog who gets super powers",
-//         "year": 2007,
-//         "quantity": 3,
-//         "imageURL": "https://imgur.com/cEJmGKV.jpg"
-//       },
-//       {
-//         "title": "The Annals of Arathrae",
-//         "description": "This anthology tells the intertwined narratives of six fairy tales.",
-//         "year": 2016,
-//         "quantity": 8,
-//         "imageURL": "https://imgur.com/VGyUtrr.jpeg"
-//       },
-//       {
-//         "title": "Wâˆ€RP",
-//         "description": "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
-//         "year": 2010,
-//         "quantity": 4,
-//         "imageURL": "https://imgur.com/qYLKtPH.jpeg"
-//       }])
-//         .then(res.status(200).json({
-//             message: 'Seed successful'
-//         }))
-//         .catch(res.status(400).json({
-//             message: 'Seed unsuccessful'
-//         }))
-// })
+//Index
+router.get('/', (req, res) => {
+    Books.find()
+    .then(foundBooks => {
+        res.send(foundBooks)
+    })
+    .catch(err => {
+        res.send(err)
+    })
+})
 
 //SHOW
 router.get('/id', (req,res) => {
     let id = req.params.id
     Books.findByIdAndUpdate(id, req.body, {new: true})
-    .then(updatedBook => {
-        console.log(updatedBook)
+    .then(foundBook => {
+        console.log(foundBook)
         res.redirect('/books')
     })
 })
@@ -78,17 +52,17 @@ router.delete('/:id', (req, res) => {
     })
 })
 
-//Error 404
-router.get('/', (req, res) => {
-    books.find()
-    .then(places => {
-        res.send(places)
-    })
-    .catch(err => {
-        console.log('err', err)
-        res.render('error404')
-    })
-})
+// //Error 404
+// router.get('/', (req, res) => {
+//     books.find()
+//     .then(places => {
+//         res.send(places)
+//     })
+//     .catch(err => {
+//         console.log('err', err)
+//         res.render('error404')
+//     })
+// })
 
 
 
